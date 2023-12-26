b8bf3cc8aaafd52b6c71bcd89b3a53dc91d6b50f
Marvin <okongomarvin971@gmail.com>
2023-12-26 14:09:04 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task.value)
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
